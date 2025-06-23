6b9bed6e17a3862d946a01230fcbe2312e427232
Nidhal-Abidi <nidhalabidi201@gmail.com>
2025-06-23 11:43:30 +0200

    add loading state for the button during payment

--- src/components/payment/creditCard/CardPaymentButton.tsx
+++ src/components/payment/creditCard/CardPaymentButton.tsx
@@ -1,19 +1,27 @@
 import { Tier } from "@/utils/constants";
 import { paymentButtonColor } from "@/utils/utilites";
+import { useFormStatus } from "react-dom";
 
 interface CardPaymentButtonProps {
   selectedTier: Tier;
 }
 
 export function CardPaymentButton({ selectedTier }: CardPaymentButtonProps) {
+  const { pending } = useFormStatus();
+
   return (
     <button
       type="submit"
+      disabled={pending}
       className={`${paymentButtonColor(
         selectedTier.id
-      )} text-white font-bold mt-8  px-8 py-3 rounded-lg cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors shadow-lg `}
+      )}  text-white font-bold mt-8 px-8 py-3 rounded-lg cursor-pointer transition-colors shadow-lg ${
+        pending
+          ? "bg-gray-300 text-gray-600 cursor-not-allowed shadow-none"
+          : "hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2"
+      }`}
     >
-      Pay {selectedTier.price} Now
+      {pending ? "Paying ..." : `Pay ${selectedTier.price} Now`}
     </button>
   );
 }
