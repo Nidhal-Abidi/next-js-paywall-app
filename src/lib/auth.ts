ee58ad1a0c966fe88aa58da38901faf8f9df4284
Nidhal-Abidi <nidhalabidi201@gmail.com>
2025-07-22 19:14:29 +0200

    feat: fix GitHub OAuth configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,7 +14,10 @@ export const { handlers, auth, signIn } = NextAuth({
     strategy: "jwt",
   },
   providers: [
-    GitHub,
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
     Credentials({
       credentials: {
         email: {},
